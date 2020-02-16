7805a82281646d0c08bfed08832f23de2a54a051
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-02-16 18:27:43 +0300

    import to git. Part one

---
+++ Launcher/Infrastructure/Interfaces/ISerializer.cs
@@ -0,0 +1,8 @@
+﻿namespace Launcher {
+    public interface ISerializer {
+        void Serialize(string FileName, object value);
+        T Deserialize<T>(string FileName);
+        object Deserialize(string FileName);
+        string GetPath(string FileName);
+    }
+}
---
+++ Launcher/Infrastructure/Serializers/DefaultSerializer.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Launcher {
+    public class DefaultSerializer : ISerializer {
+
+        public void Serialize(string FileName, object value) {
+            string path = GetPath(FileName);
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            using (StreamWriter file = new StreamWriter(fs)) {
+                JsonSerializer serializer = new JsonSerializer {
+                    Formatting = Formatting.Indented,
+                    TypeNameHandling = TypeNameHandling.Auto
+                };
+                serializer.Serialize(file, value);
+            }
+        }
+        public T Deserialize<T>(string FileName) {
+            T obj;
+            string path = GetPath(FileName);
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            using (StreamReader file = new StreamReader(fs)) {
+                JsonSerializer serializer = new JsonSerializer {
+                    Formatting = Formatting.Indented,
+                    TypeNameHandling = TypeNameHandling.Auto
+                };
+
+                obj = (T)serializer.Deserialize(file, typeof(T));
+            }
+            return obj;
+        }
+        public object Deserialize(string FileName) {
+            object obj;
+            string path = GetPath(FileName);
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            using (StreamReader file = new StreamReader(fs)) {
+                string json = file.ReadToEnd();
+                obj = JsonConvert.DeserializeObject(json, new JsonSerializerSettings {
+                    Formatting = Formatting.Indented,
+                    TypeNameHandling = TypeNameHandling.Auto,
+                });
+                return obj;
+            }
+        }
+
+        public string GetPath(string fileName) {
+            if (!Directory.Exists("Users")) {
+                Directory.CreateDirectory("Users");
+            }
+
+            string path = Path.Combine("Users", ( Path.ChangeExtension(fileName, "json")));
+            return path;
+        }
+    }
+}
