0c106b02ba203a366c1038cb79d0b3dd910515cc
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-02-16 20:27:42 +0300

    import to git. Part four. ViewModel

---
+++ Launcher/ViewModel/ProjectVM/RemoveProject.cs
@@ -0,0 +1,24 @@
+﻿using Launcher.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher.ViewModel {
+    class RemoveProject : ProjectCommand {
+        protected ProjectReceiver receiver;
+        protected Project project;
+        public RemoveProject( ProjectReceiver receiver, Project project) {
+            this.receiver = receiver;
+            this.project = project;
+        }
+        public override void Execute() {
+            receiver.Action();
+        }
+
+        public override void UnExecute() {
+            throw new NotImplementedException();
+        }
+    }
+}
