89774937a5a393065bbfc9b32b99cfb123bdf640
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-02-16 22:51:03 +0300

    add JSON file validation

--- Launcher/Model/User/BuilderForUser/JsonUserBuilder.cs
+++ Launcher/Model/User/BuilderForUser/JsonUserBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 
 namespace Launcher.Model.BuilderForUser {
     internal class JsonUserBuilder : UserBuilder {
@@ -13,23 +14,31 @@ namespace Launcher.Model.BuilderForUser {
             user = new User();
         }
         public override void SetUserInformation() {
-            user = json.Deserialize<User>(username);
+            if (File.Exists($"{username}.json")) {
+                user = json.Deserialize<User>(username);
+            }
+            throw new FileNotFoundException($"{username}.json not found!");
             //дополнительные свойства
         }
         public override void SetProjects() {
-
-            ObservableCollection<Project> projectsFromFile = json.Deserialize<ObservableCollection<Project>>(user.Name + "'s_projects");
-
-            foreach (var item in projectsFromFile) {
-                user.ProjectCollection.AddProject(item);
+            string projects = user.Name + "'s_projects";
+            if (File.Exists($"{projects}.json")) {
+                ObservableCollection<Project> projectsFromFile = json.Deserialize<ObservableCollection<Project>>(projects);
+                foreach (var item in projectsFromFile) {
+                    user.ProjectCollection.AddProject(item);
+                }
             }
-
+            throw new FileNotFoundException($"{projects}.json not found!");
         }
         public override void SetMaterials() {
-            Dictionary<string, Material> usefulMaterialsFromFile = json.Deserialize<Dictionary<string, Material>>(user.Name + "'s_usefulMaterials");
-            foreach (var item in usefulMaterialsFromFile) {
-                user.UsefulMaterials.Add(item.Value);
+            string projects = user.Name + "'s_usefulMaterials";
+            if (File.Exists($"{projects}.json")) {
+                Dictionary<string, Material> usefulMaterialsFromFile = json.Deserialize<Dictionary<string, Material>>(projects);
+                foreach (var item in usefulMaterialsFromFile) {
+                    user.UsefulMaterials.Add(item.Value);
+                }
             }
+            throw new FileNotFoundException($"{projects}.json not found!");
         }
         public override User GetUser() {
             return user;
