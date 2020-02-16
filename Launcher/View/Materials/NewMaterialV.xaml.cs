4c8d5faafa62c1f9bfe2953a8efea0231baba2dc
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-02-16 20:24:55 +0300

    import to git. Part three. View

---
+++ Launcher/View/Materials/NewMaterialV.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Launcher.ViewModel;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Launcher.View {
+    /// <summary>
+    /// Логика взаимодействия для AddMaterialV.xaml
+    /// </summary>
+    public partial class NewMaterialV : Window, IDisposable {
+        public NewMaterialV(MaterialVM vm) {
+
+            DataContext = vm;
+            vm.ChangeDialogResult += ChangeDialogResultMethod;
+            InitializeComponent();
+        }
+
+        // событие которое опевестит вью о закрытии и изменении занчения DialogResult
+        private void ChangeDialogResultMethod() {
+            this.DialogResult = true;
+            this.Close();
+        }
+        public void Dispose() {
+            // throw new NotImplementedException();
+        }
+        private void MaterialTitle_tb_GotFocus(object sender, RoutedEventArgs e) {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= MaterialTitle_tb_GotFocus;
+        }
+    }
+}
---
+++ Launcher/View/ProjectV.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Launcher.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Launcher.View {
+    /// <summary>
+    /// Логика взаимодействия для ProjectView.xaml
+    /// </summary>
+    public partial class ProjectV : Page {
+        public ProjectV(ProjectVM vm) {
+
+            DataContext = vm;
+            InitializeComponent();
+        }
+        private void ToChangeMaterials_Click(object sender, System.Windows.RoutedEventArgs e) {
+            bool EditingEnabled = ( sender as ToggleButton ).IsChecked ?? false;
+
+            int indexRemoveProjectMaterial = ProjectMaterials_DG.Columns.IndexOf(removeProjectMaterial_column);
+
+            if (EditingEnabled) {
+                ProjectMaterials_DG.Columns[indexRemoveProjectMaterial].Visibility = Visibility.Visible;
+                panelForAddMaterials.Visibility = Visibility.Visible;
+            }
+            else {
+                ProjectMaterials_DG.Columns[indexRemoveProjectMaterial].Visibility = Visibility.Hidden;
+                panelForAddMaterials.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ToChangeTasks_Click(object sender, System.Windows.RoutedEventArgs e) {
+            bool EditingEnabled = ( sender as ToggleButton ).IsChecked ?? false;
+
+            int indexNameTask = Task_DG.Columns.IndexOf(NameTask_colum);
+            int indexDeleteTask = Task_DG.Columns.IndexOf(deleteTask_column);
+
+            if (EditingEnabled) {
+                Task_DG.Columns[indexDeleteTask].Visibility = Visibility.Visible;
+                panelForAddTasks.Visibility = Visibility.Visible;
+            }
+            else {
+                Task_DG.Columns[indexDeleteTask].Visibility = Visibility.Hidden;
+                panelForAddTasks.Visibility = Visibility.Hidden;
+            }
+            Task_DG.Columns[indexNameTask].IsReadOnly = !Task_DG.Columns[indexNameTask].IsReadOnly;
+        }
+
+       
+
+     
+    }
+}
