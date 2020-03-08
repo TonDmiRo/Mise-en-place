c0a822ec9e99fee4d40c4aef6143bc1ae893e126
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-03-08 16:24:55 +0300

    Implement IDisposable
    После закрытия loginV в памяти сохранялся DataContext = LoginVM

--- Launcher/View/LoginV.xaml.cs
+++ Launcher/View/LoginV.xaml.cs
@@ -1,12 +1,16 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Launcher.View {
     /// <summary>
     /// Логика взаимодействия для Login.xaml
     /// </summary>
-    public partial class LoginV : Window {
+    public partial class LoginV : Window, IDisposable {
         public LoginV() {
             InitializeComponent();
+        }
+        public void Dispose() {
+            DataContext = null;
         }
     }
 }
