fb15bda417333b9dcfe78532b9d93ae453577587
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-03-16 18:29:03 +0300

    Add DoingV

---
+++ Launcher/View/DoingV.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Launcher.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Launcher.View {
+    /// <summary>
+    /// Interaction logic for DoingV.xaml
+    /// </summary>
+    public partial class DoingV : Window,IDisposable {
+       
+        public DoingV(DoingVM vm) {
+            DataContext = vm;
+            InitializeComponent();
+        }
+
+        public void Dispose() {
+            DataContext = null;
+        }
+    }
+}
---
+++ Launcher/ViewModel/DoingVM.cs
@@ -0,0 +1,54 @@
+﻿using Launcher.Model;
+using System;
+namespace Launcher.ViewModel {
+    public class DoingVM : BaseVM {
+
+        public DoingVM() {
+            magnifierForElapsedTime = new TimeSpanMagnifier(TimeSpan.Zero, ChangedUsageTimeTotal);
+            magnifierForElapsedTime.MillisecondsForTimer = 1000;
+        }
+
+        private void ChangedUsageTimeTotal(object sender, TimeSpanHasChangedEventArgs e) {
+            ElapsedTime = e.IncreasedTimeSpan;
+        }
+        public TimeSpan ElapsedTime {
+            get => elapsedTime;
+            set {
+                elapsedTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private TimeSpanMagnifier magnifierForElapsedTime;
+
+        public bool TwentyFiveMinutesPassed {
+            get => twentyFiveMinutesPassed;
+            set {
+                twentyFiveMinutesPassed = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool FiftyMinutesPassed {
+            get => fiftyMinutesPassed;
+            set {
+                fiftyMinutesPassed = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool TimeToRelax {
+            get => timeToRelax;
+            set {
+                timeToRelax = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #region private
+        private TimeSpan elapsedTime;
+        private bool twentyFiveMinutesPassed;
+        private bool fiftyMinutesPassed;
+        private bool timeToRelax;
+        #endregion
+    }
+}
