442b634ecd7f512aa1681b954d29ad9b858ead7e
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-03-18 20:42:31 +0300

    Complete construction DoingVM

--- Launcher/ViewModel/DoingVM.cs
+++ Launcher/ViewModel/DoingVM.cs
@@ -1,25 +1,44 @@
-﻿using Launcher.Model;
-using System;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
 namespace Launcher.ViewModel {
     public class DoingVM : BaseVM {
 
         public DoingVM() {
-            magnifierForElapsedTime = new TimeSpanMagnifier(TimeSpan.Zero, ChangedUsageTimeTotal);
-            magnifierForElapsedTime.MillisecondsForTimer = 1000;
+            magnifierForElapsedTime = new TimeSpanMagnifier(TimeSpan.Zero, ChangedUsageTimeTotal) {
+                MillisecondsForTimer = 1000
+            };
         }
 
         private void ChangedUsageTimeTotal(object sender, TimeSpanHasChangedEventArgs e) {
             ElapsedTime = e.IncreasedTimeSpan;
         }
+
         public TimeSpan ElapsedTime {
             get => elapsedTime;
-            set {
+            private set {
                 elapsedTime = value;
                 OnPropertyChanged();
+                _counterToCheck++;
+
+                if (_counterToCheck > 60) {
+                    _counterToCheck = 0;
+                    CheckTime();
+                }
             }
         }
-
-        private TimeSpanMagnifier magnifierForElapsedTime;
+        private void CheckTime() {
+            if (ElapsedTime.TotalMinutes < 60) {
+                if (ElapsedTime.Minutes < 50) {
+                    if (ElapsedTime.Minutes > 25) {
+                        TwentyFiveMinutesPassed = true;
+                    }
+                }
+                else { FiftyMinutesPassed = true; }
+            }
+            else { TimeToRelax = true; }
+        }
 
         public bool TwentyFiveMinutesPassed {
             get => twentyFiveMinutesPassed;
@@ -43,12 +62,34 @@ namespace Launcher.ViewModel {
             }
         }
 
+        #region Commands
+        private ICommand _skipTaskCommand;
+        public ICommand SkipTaskCommand => _skipTaskCommand ?? ( _skipTaskCommand = new RelayCommand(CloseDoingV) );
 
+        private ICommand _completeTaskCommand;
+        public ICommand CompleteTaskCommand => _completeTaskCommand ?? ( _completeTaskCommand = new RelayCommand(CloseDoingV, CanDone) );
+        private void CloseDoingV(object parameter) {
+            Window window = (Window)parameter;
+            window.Close();
+        }
+        private bool CanDone(object parameter) {
+            return TwentyFiveMinutesPassed || FiftyMinutesPassed || TimeToRelax;
+        }
+        #endregion
         #region private
         private TimeSpan elapsedTime;
+        private TimeSpanMagnifier magnifierForElapsedTime;
+
         private bool twentyFiveMinutesPassed;
         private bool fiftyMinutesPassed;
         private bool timeToRelax;
+        private int _counterToCheck;
         #endregion
+
+        public override void Dispose() {
+            base.Dispose();
+            magnifierForElapsedTime.Dispose();
+            magnifierForElapsedTime = null;
+        }
     }
 }
