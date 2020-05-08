2690127c489e014d156e201bf45ce825f9e386da
DmiRo <61103969+TonDmiRo@users.noreply.github.com>
2020-05-08 18:21:05 +0300

    Add open command for materials

--- Launcher/ViewModel/Pages/ProjectMaterialsPageForNav.cs
+++ Launcher/ViewModel/Pages/ProjectMaterialsPageForNav.cs
@@ -4,34 +4,39 @@ using System.Windows.Input;
 
 namespace Launcher.ViewModel.Pages {
     internal class ProjectMaterialsPageForNav : BasePageVM {
-
-
         public string NewMaterialTitle {
             get => _title;
             set {
                 _title = value;
-                OnPropertyChanged();
+                // OnPropertyChanged();
             }
         }
-
 
         public Material SelectedMaterial {
             get { return _selectedMaterial; }
             set {
                 _selectedMaterial = value;
+                NewMaterialTitle = ( value != null ) ? value.MaterialTitle : string.Empty;
                 // OnPropertyChanged();
 
             }
         }
 
-
+        #region Commands
+        private ICommand _openMaterialCommand;
+        public ICommand OpenMaterialCommand => _openMaterialCommand ?? ( _openMaterialCommand = new RelayCommand(OpenMaterial) );
+        private void OpenMaterial(object parameter) {
+            if (parameter is Material material) {
+                material.OpenMaterial();
+            }
+        }
 
         private ICommand _removeSelectedMaterialCommand;
         public ICommand RemoveSelectedMaterialCommand => _removeSelectedMaterialCommand ?? ( _removeSelectedMaterialCommand = new RelayCommand(RemoveSelectedMaterial) );
-
         private void RemoveSelectedMaterial(object parameter) {
             Project.Materials.Remove(SelectedMaterial);
         }
+        #endregion
 
         public ProjectMaterialsPageForNav() {
             Project = Project.EmptyProject;
@@ -41,7 +46,6 @@ namespace Launcher.ViewModel.Pages {
             Project.Materials.Add(new LocalMaterialCreator().CreateMaterial("Material2", "\\help.txt"));
             Project.Materials.Add(new LocalMaterialCreator().CreateMaterial("Material3", "\\help.txt"));
         }
-
 
         public ProjectMaterialsPageForNav(Action<object> onGoPage, Func<object, bool> canGoPage) : base(onGoPage, canGoPage) { }
 
